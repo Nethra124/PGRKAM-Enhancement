dcc186330531231f14c126a8e6a856fe591f53b0
22arun11 <arunkumar22112003@gmail.com>
2023-10-14 11:18:05 +0530

    Initialising

---
+++ hackathon/myapp/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns =[
+    path('',views.index,name='index'),
+    path('index.html',views.index,name='index'),
+    path('404.html',views.fun404,name='fun404'),
+    path('about.html',views.about,name='about'),
+    path('category.html',views.category,name='category'),
+    path('contact.html',views.contact,name='contact'),
+    path('job-list.html',views.joblist,name='joblist'),
+    path('testimonial.html',views.testimonial,name='testimonial'),
+    path('job-detail.html',views.jobdetail,name='jobdetail'),
+    path('chat.html',views.chat,name='chat'),]
+
+
